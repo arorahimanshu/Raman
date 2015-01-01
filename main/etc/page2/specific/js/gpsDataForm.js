d211c134818caf757263fef7982633f9343c78e5
Himanshu Arora <ihimanshuarora@yahoo.com>
2015-01-02 00:33:37 +0530

    server offset fix + tabs box changed

--- main/etc/page2/specific/js/gpsDataForm.js
+++ main/etc/page2/specific/js/gpsDataForm.js
@@ -4,7 +4,7 @@ fitx.utils.require(['fitx', 'page2', 'newGpsDataFormAction']);
 jQuery(window).load(function() {
 	/************************Control Related**********************/
 	makeMapAnimator()
-	var dataTimer = 5			//in seconds
+	var dataTimer = 20			//in seconds
 	startNewAnimation()
 	startNewAnimation.every(dataTimer * 1000)
 	
